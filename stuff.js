84c5bc48a448c98cd5cde7ce8b4425b5a708173a
rbommann <rbommann@central.uh.edu>
2017-05-05 10:41:03 -0500

    add a condition in javascript file

--- stuff.js
+++ stuff.js
@@ -2,5 +2,14 @@ function myFunction() {
 
   $name = document.getElementById('text').value;
   document.getElementById('display').innerHTML = $name;
-}
+
+  if ($name !== '') {
+    alert('in if condition');
+    document.getElementById('display').innerHTML = $name;
+  } else {
+    alert('outside else');
+    $data = 'Field is empty.please enter the data';
+    document.getElementById('display').innerHTML = $data;
+  }
+
 }
